b38d9939a9fe51ba43e03cb3f134facdb5d0ef50
Dmytro Shkolyar <d.shkolyar7@gmail.com>
2025-10-08 14:40:53 -0300

    feature/create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity'; // connect entity with app module
 
 @Module({
   // here we will connect the databse to our app module
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'postgres', // here we set what db are we using
       host: 'localhost',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'focal_nest_db',
-      entities: [], //here we will add our entities
-      synchronize: true,
+      database: 'focal_nest_db', // our db name
+      entities: [User], //here we will add our entities
+      // this is thanks to typeorm
+      synchronize: true, // this is only used at the dev env. (it creates tables automatically in the db, without writing migrations
     }),
     UsersModule,
   ],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], //this line creates the repository for User entity
   controllers: [UsersController],
   providers: [UsersService],
 })
